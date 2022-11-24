c275762f99cc4d0af0d4fb30fa7df5064197702e
SebJohansen8763 <113422792+SebJohansen8763@users.noreply.github.com>
2022-11-24 13:27:07 +0100

    c

---
+++ include/duration.hpp
@@ -0,0 +1,16 @@
+
+#ifndef _DURATION_H_
+#define _DURATION_H_
+
+class Duration {
+
+	private:
+
+	int time;
+
+	public:
+
+	Duration();
+	 ~Duration();
+};
+#endif
---
+++ src/duration.cpp
@@ -0,0 +1,5 @@
+#include "duration.hpp"
+
+int duration::(){
+
+}
