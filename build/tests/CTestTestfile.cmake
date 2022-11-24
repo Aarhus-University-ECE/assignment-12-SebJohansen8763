4ee21a4639904b9313e952ba57048780523773e5
SebJohansen8763 <113422792+SebJohansen8763@users.noreply.github.com>
2022-11-24 12:43:25 +0100

    cmake

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sebastianjohansen/Documents/GitHub/assignment-12-SebJohansen8763/tests
+# Build directory: /Users/sebastianjohansen/Documents/GitHub/assignment-12-SebJohansen8763/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(usage_test "tests")
+set_tests_properties(usage_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/sebastianjohansen/Documents/GitHub/assignment-12-SebJohansen8763/tests/CMakeLists.txt;6;add_test;/Users/sebastianjohansen/Documents/GitHub/assignment-12-SebJohansen8763/tests/CMakeLists.txt;0;")
--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "duration.hpp"
 
 
 // See Catch2's documentation: https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#scaling-up
